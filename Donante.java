3fc8548d86d9c857bfde96c7a472d0bdf26a992c
The_peeck <iguardado158@gmail.com>
2025-09-10 09:09:53 -0600

    Update Donante.java

--- Donante.java
+++ Donante.java
@@ -1,17 +1,19 @@
-//falta clase
-import java.util.ArrayList;
-//hereda métodos de la clase Uusuario
-public class Donante extends Usuario{
-    private ArrayList<String> donaciones;
+//crea la clase de Donante
+class Donante extends Usuario {
+    private List<String> donaciones;
+//crea la informacion para el donante 
     public Donante(String nombre, String correo, String contrasena, String ubicacion) {
         super(nombre, correo, contrasena, ubicacion);
         this.donaciones = new ArrayList<>();
     }
-
-    public void setDonaciones(String newdonacion){
-
-    }
-    public ArrayList<String> getDonaciones(){
+//crea la lista para las donaciones
+    public List<String> getDonaciones() {
         return donaciones;
     }
+//se agrega las donaciones en la lista
+    public void agregarDonacion(String donacion) {
+        this.donaciones.add(donacion);
+    }
 }
+//quite getDonacioneRealizadas, ya que es lo mismo que hace getDonaciones, y seria tonto dejarlo
+//se pusieron las listas para que sea mas facil encontrar las donaciones 
