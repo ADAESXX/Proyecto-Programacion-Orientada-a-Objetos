e41383485a5a66716c7dee82b5748cde7fd7463f
Allyson Escobar <esc25862@uvg.edu.gt>
2025-10-23 16:51:47 -0600

    Update Emprendedor.js

--- Js version/Emprendedor.js
+++ Js version/Emprendedor.js
@@ -1,17 +1,36 @@
-// models/Emprendedor.js
-// progamador: José Ignacio Guardado
-import Usuario from "./Usuario.js";
-
-export default class Emprendedor extends Usuario {
-  constructor(id, nombre, correo, contrasena, empresa, sector) {
-    super(id, nombre, correo, contrasena, "Emprendedor");
-    this.empresa = empresa;
-    this.sector = sector;
+class Emprendedor extends Usuario {
+  constructor(nombre, correo, contrasena, ubicacion, nombreProyecto) {
+    super(nombre, correo, contrasena, ubicacion);
+    this.setNombreProyecto(nombreProyecto);
+    this.proyectos = [];
   }
 
-  getEmpresa() { return this.empresa; }
-  getSector() { return this.sector; }
+  getNombreProyecto() {
+    return this.nombreProyecto;
+  }
 
-  setEmpresa(empresa) { this.empresa = empresa; }
-  setSector(sector) { this.sector = sector; }
+  setNombreProyecto(nombreProyecto) {
+    if (nombreProyecto == null || nombreProyecto.trim() === "") {
+      throw new Error("El nombre del proyecto no puede estar vacío");
+    }
+    this.nombreProyecto = nombreProyecto.trim();
+  }
+
+  getProyectos() {
+    return this.proyectos;
+  }
+
+  setProyectos(proyectos) {
+    if (proyectos == null) {
+      throw new Error("La lista de proyectos no puede estar vacía");
+    }
+    this.proyectos = proyectos;
+  }
+
+  agregarProyectos(proyecto) {
+    if (proyecto == null || proyecto.trim() === "") {
+      throw new Error("El proyecto no puede ser vacío o nulo");
+    }
+    this.proyectos.push(proyecto);
+  }
 }
